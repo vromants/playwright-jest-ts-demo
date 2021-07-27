2f30b55f3f76eacf3c9ba859f310a956aa29405c
vromants <vitaliyromantsov@gmail.com>
2021-07-27 19:43:31 +0300

    initial set up

---
+++ jest-playwright.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  browsers: ["chromium", "firefox", "webkit"],
+  exitOnPageError: false, // GitHub currently throws errors
+  launchOptions: {
+    headless: false
+  }
+}
---
+++ jest.config.ts
@@ -0,0 +1,3 @@
+export = {
+  preset: "jest-playwright-preset"
+}
